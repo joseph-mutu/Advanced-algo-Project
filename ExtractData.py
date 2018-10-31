6e97ff0c2cccee826e8f2744c06f568fcb982420
nmutudeH <josephmathone@gmail.com>
2018-10-31 16:03:07 +0100

    Repro Extract into a function to Preprocess data

--- ExtractData.py
+++ ExtractData.py
@@ -5,35 +5,39 @@
 # @Link    : ${link}
 # @Version : $Id$
 
-# 以下将尝试打开 tsp 文件，将文件保存在元组中
 import numpy as np
 
-with open('D:/Study/Jean Monnet/Advanced Algoirthm/Project/Data/a280.tsp') as f:
-	
-	num = 0 #统计城市数目
-	find_num = False # 标记找到的下一行即为真实城市数据
-	# corMat = np.array()
-	temMat = []
-	# corMat = [] #空列表，保存城市的坐标
-	data = f.readlines()
-
-	for line in data:
-		line = line.rstrip('\n') # 读取文件时逐行读取的话，在每一行的末尾会录入一个 \n 换行符
+def PreData():
+#此函数将获取一个文件名，文件名为 TSP 数据 ----  此项尚未完成
+#并将读取数据最终输出城市坐标以及城市数目
+	with open('D:/Study/Jean Monnet/Advanced Algoirthm/Project/Data/a280.tsp') as f:
 		
-		if find_num:
-			string_data = line.split()
-			temMat.append(string_data)
-			# tem = np.array(string_data)
-			# real_data = map(float, string_data)
-			# print(real_data)
-			# corMat[num][0:2] = real_data
-			num = num + 1		
-		
-		if line == "NODE_COORD_SECTION":
-			find_num = True
+		num = 0 #统计城市数目
+		find_num = False # 标记找到的下一行即为真实城市数据
+		temMat = []
+		data = f.readlines()
 
-	temMat = temMat[0:num-1]
+		for line in data:
+			line = line.rstrip('\n') # 读取文件时逐行读取的话，在每一行的末尾会录入一个 \n 换行符
+			
+			if find_num:	
 
-	corMat = np.array(temMat)
-	corMat = corMat.astype(np.float64)
-print(corMat[0][2])
+				#将数据按照空格分开
+				string_data = line.split()
+				temMat.append(string_data)
+
+				num = num + 1		#统计城市数目
+			
+			if line == "NODE_COORD_SECTION":
+				# 在 NODE_COORD_SECTION 的下一行即为真实数据
+				find_num = True
+
+# 读取到的文件最后一行有 EOF 标识
+		temMat = temMat[0:num-1]
+#将 List 数据转化为 array 数据
+		corMat = np.array(temMat)
+		corMat = corMat.astype(np.float64)
+# 其中 corMat 即为城市坐标矩阵，而 num 则为城市数目
+		return corMat,num-1;
+
+# CorMat, num_cities = PreData()
